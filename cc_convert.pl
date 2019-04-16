b957fcb0a8c9c23c82f4e07041b9163ebd299dea
Wadim Mueller <wadim.mueller@continental-corporation.com>
2019-04-16 22:42:28 +0200

    add bear vs. cmake style + ghs detection

--- cc_convert.pl
+++ cc_convert.pl
@@ -4,6 +4,8 @@
 # Created: 15 Apr 2019
 # Version: 0.01
 
+use File::Basename;
+use Getopt::Long qw(GetOptionsFromString);
 use warnings;
 use strict;
 
@@ -11,23 +13,23 @@ my $filename = 'compile_commands.json';
 open(my $fh, '<:encoding(UTF-8)', $filename)
     or die "Could not open file '$filename' $!";
 
-my %ghs_mapping = (
-    "a" => "b"
-);
-
-my %gnu_mapping = (
-    "a" => "b",
-    "b" => "c"
-);
 
 my %mapping = (
-    "cxxarm:cxxarm64" => \%ghs_mapping,
-    "cc:gcc:g++" => \%gnu_mapping,
+    "ccintarm64:cxintarm64" => \&translate_to_ghs_options,
+    "cc:gcc:g++" => \&translate_to_ghs_options,
 );
 
 my %compile_args;
 
-sub get_map_table_from_cc {
+sub translate_to_ghs_options {
+    my $cc = $_[0];
+    my $options = $_[1];
+    #unshift @$options, 'clang';
+    print "@$options \n\n --> converted to: \n\n";
+    #$ret = GetOptionsFromString($string, ...);
+}
+
+sub get_translator {
     my $cc = $_[0];
     my @cc_names = keys %mapping;
 
@@ -42,23 +44,42 @@ sub get_map_table_from_cc {
     return 0;
 }
 
+my $bear_style = 0;
+my $count = 0;
 
 while (my $row = <$fh>) {
     my $ccarg;
     my @file_args;
     chomp $row;
-    if ( $row =~ /\s*\"arguments\s*\":\s*\[/i) {
-        do{
-            $ccarg = <$fh>;
-            if ($ccarg =~ /\s*\"(.+)\"/ ) {
-                push @file_args, $1;
+    if ($bear_style == 1) {
+        if ( $row =~ /\s*\"arguments\s*\":\s*\[/i) {
+            do{
+                $ccarg = <$fh>;
+                if ($ccarg =~ /\s*\"(.+)\"/ ) {
+                    push @file_args, $1;
+                }
+            } while ($ccarg !~ /\s*\],/i);
+            my $compilation_unit = pop @file_args;
+            $compile_args{$compilation_unit} = join(" ",@file_args);
+            my $ret = get_translator($file_args[0]);
+        }
+    } else {
+        if ( $row =~ /\s*\"command\s*\":\s*\"(.*)\"/i) {
+            my @options = split(" ", $1);
+            foreach (@options) {
+                $_ =~ s/\\\\/\//g;
             }
-        } while ($ccarg !~ /\s*\],/i);
-        my $compilation_unit = pop @file_args;
-        $compile_args{$compilation_unit} = join(" ",@file_args);
-        my $ret = get_map_table_from_cc($file_args[0]);
-        if ( $ret ) {
-            print $ret->{"b"} . "\n";
+            my $compiler = shift(@options);
+            my $base_name_compiler = fileparse($compiler,".exe","");
+            my $translator = get_translator($base_name_compiler);
+            if ($translator) {
+                $translator->($base_name_compiler, \@options);
+            } else {
+                print "No Translator found for CC = $base_name_compiler\n";
+            }
+            #print "$count $base_name_compiler : @options";
+            $count++;
+            exit;
         }
     }
 }
